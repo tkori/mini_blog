6b75c24898de205195f0211f68014d1b109bd680
tkori <tkori19+github@gmail.com>
2019-07-11 13:49:11 +0900

    Delete the description of pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,28 +2,20 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /articles
-  # GET /articles.json
   def index
-    @articles = Article.all.order("created_at DESC").page(params[:page]).per(10)
+    @articles = Article.all.order("created_at DESC")
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
   end
 
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
 
@@ -38,8 +30,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -52,8 +42,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     @article.destroy
     respond_to do |format|
@@ -63,12 +51,10 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :content).merge(user_id: current_user.id)
     end
